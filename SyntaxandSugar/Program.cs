1f7d5d23b918c5d43a610b9e03953fbd79d4ad58
rajvi rajurkar <rajvi.vm@gmail.com>
2022-06-22 21:43:23 -0700

    I have updated

---
+++ SyntaxandSugar/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SyntaxandSugar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //int answer = 4;
+            //string response;
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + " is greater than or equal to nine";
+            //}
+
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater tha or equal to nine";
+      
+            Console.WriteLine(response);
+
+        }
+    }
+}
+
